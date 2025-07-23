8bd70a9c3ddc9675cf9fe98b47429d5d18a15fb0
Tushar Kumar <tushark81940@gmail.com>
2025-07-23 23:27:52 +0530

    m

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,6 +4,6 @@ export async function POST(req : NextRequest){
     const body = await req.json();
     console.log(body);
     return Response.json({
-        message: "User created successfully",   
+        message: "User created successfully !",   
 })
 }
